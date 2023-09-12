4a542283b1cfb009aacc624ca15c14278c7480ea
FelipOliveira <sem.ideias.na.hora@gmail.com>
2023-09-12 18:25:15 -0300

    first commit

---
+++ src/main/java/com/br/foliveira/mymagic/MymagicApplication.java
@@ -0,0 +1,13 @@
+package com.br.foliveira.mymagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymagicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/foliveira/mymagic/MymagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.foliveira.mymagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
